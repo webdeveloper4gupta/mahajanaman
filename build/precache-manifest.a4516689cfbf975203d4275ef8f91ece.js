9494a0e2f806fc63bb2da657edc308d1a88d5ffa
SUKRITAN GUPTA <20051871@kiit.ac.in>
2022-02-24 21:21:49 +0530

    this is my react app
    this is my first react project

---
+++ build/precache-manifest.a4516689cfbf975203d4275ef8f91ece.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dffc9bc545d73e554c6e",
+    "url": "/build/static/css/main.a55584ba.chunk.css"
+  },
+  {
+    "revision": "dffc9bc545d73e554c6e",
+    "url": "/build/static/js/main.dffc9bc5.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/build/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/build/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "f35fe0376a379e6ef37e",
+    "url": "/build/static/js/runtime~main.f35fe037.js"
+  },
+  {
+    "revision": "5691fa96111638d0b487a480c28a4021",
+    "url": "/build/index.html"
+  }
+];
